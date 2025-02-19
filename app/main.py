a0525ebb09f851950bdace7638f798187bfa7e2a
GoldenLin9 <goldenlin0909@gmail.com>
2025-02-19 16:36:40 -0500

    implements youtube video retrieval function

--- app/main.py
+++ app/main.py
@@ -3,6 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 from app.core.config import settings
 
+import os
+import googleapiclient.discovery
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = FastAPI()
 
 origins = [
@@ -27,3 +34,43 @@ async def root():
         "message": "Welcome to the RCE Autocomplete API",
         "version": "1.0.0",
     }
+    
+def youtube(query: str):
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+
+    print("my yt key: ", os.getenv("YOUTUBE_API_KEY"))
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = os.getenv("YOUTUBE_API_KEY")
+    
+
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    request = youtube.search().list(
+        part="snippet",
+        q=query,
+        type="video"
+    )
+    response = request.execute()
+    items = response["items"]
+
+    videos_info = {
+        "videos": []
+    }
+
+    for item in items:
+        snippet = item["snippet"]
+        print("item idddd: ", item["id"])
+        video_url = f"https://www.youtube.com/watch?v={item['id']['videoId']}"
+        video_info = {
+            "title": snippet["title"],
+            "description": snippet["description"],
+            "channel_title": snippet["channelTitle"],
+            "thumbnail": snippet["thumbnails"]["high"]["url"],
+            "publish_time": snippet["publishTime"],
+            "video_url": video_url
+        }
+        videos_info["videos"].append(video_info)
+
+    return videos_info
