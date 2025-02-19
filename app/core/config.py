06847c38c5779b5e3b9c2c2331d8e3233e44ad59
Evan Naderi <ev230759@ucf.edu>
2025-02-19 16:47:35 -0500

    config:

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     API_STR: str = "/api"
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
     YOUTUBE_API_KEY: str = os.getenv("YOUTUBE_API_KEY")
+    AWS_ACCESS_KEY_ID: str = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_KEY: str = os.getenv("AWS_SECRET_KEY")
+    AWS_REGION: str = os.getenv("AWS_REGION")
+    YOUTUBE_API_KEY: str = os.getenv("YOUTUBE_API_KEY")
     DEFAULT_MODEL: str = "gpt-4o-mini"
     
     class Config:
